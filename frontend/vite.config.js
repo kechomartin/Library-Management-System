9639b4beb5bfde9ef5fd76488249f403bfc7aa59
mk0 <kecho.mk@gmail.com>
2026-06-09 13:41:32 +0300

    Refactor Vite configuration to use the correct React plugin and remove Tailwind CSS integration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/react-plugin'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  
+  base: '/Library-Management-System/', 
 })
