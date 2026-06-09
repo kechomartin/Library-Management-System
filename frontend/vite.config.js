f38ab2d611c1ee34a43f12a7724905f633e4ddbf
mk0 <kecho.mk@gmail.com>
2026-06-09 13:50:46 +0300

    Fix import statement for React plugin in Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-plugin'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-  base: '/Library-Management-System/', 
+  base: '/Library-Management-System/',
 })
