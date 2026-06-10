2a50bfcbd2e404ef246f82b6d81c7998493c0b86
mk0 <kecho.mk@gmail.com>
2026-06-10 12:10:53 +0300

    Refactor Vite configuration to use command-based base logic

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Dynamic configuration base logic
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Library-Management-System/' : '/',
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: command === 'build' ? '/Library-Management-System/' : '/',
+  }
 })
